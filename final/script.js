fe5395b49259c8069032a31d8c0c157015b71fd3
yodkwtf <48durgesh.kumar@gmail.com>
2020-06-25 16:04:11 +0530

    Complete Website

--- final/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("#links").hide();
-  $("#menu-btn").click(changeNav);
-
-  function changeNav() {
-    $("#links").toggle(2000);
-    $("#menu-btn").toggleClass("turn");
-  }
-});
--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-$(document).ready(function() {
-  $(".nav-inks").show();
-  $(".nav-btn").click(changeNav);
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+// // show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+// // close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
 
-  function changeNav() {
-    $(".nav-links").toggle(2000);
-    $(".nav-btn").toggleClass("turn");
-  }
+
+
+// ********** set date ************
+// select span
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+
+
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    console.log(element);
+
+    let position = element.offsetTop - 62;
+    console.log(position);
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+    });
+  });
 });
